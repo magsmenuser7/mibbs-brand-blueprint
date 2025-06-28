492926ec4d4303de0d52c00116e7a4d676ed78bf
magsmenuser7 <magsmenuser7@gmail.com>
2025-06-28 17:24:35 +0530

    vite base set

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// createRoot(document.getElementById("root")!).render(<App />);
+
+
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { HashRouter as Router } from 'react-router-dom';
+
+// ✅ Wrap <App /> with <Router>
+createRoot(document.getElementById("root")!).render(
+  <Router>
+    <App />
+  </Router>
+);
