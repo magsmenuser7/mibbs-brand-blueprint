48e7765d8f2b11298256ea4042b29aa906909061
magsmenuser7 <magsmenuser7@gmail.com>
2025-07-03 17:52:31 +0530

    all changes added in backend

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,14 @@ const app = express();                  // <-- Initialize express
 const authRoutes = require("./routes/auth");        // <-- Import user data
 require("dotenv").config();
 
+
+const allowedOrigins = ["http://localhost:5000", "https://mibbs.ai"];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
+
 // Middleware
 app.use(cors());
 app.use(express.json());              // <-- Parse JSON bodies
@@ -30,9 +38,9 @@ app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
 
-app.get("/api/user", (req, res) => {
-  res.json(users);                    // <-- Send user data as JSON
-});
+// app.get("/api/user", (req, res) => {
+//   res.json(users);                    // <-- Send user data as JSON
+// });
 
 // Start server
 const PORT = 5000;
@@ -42,22 +50,3 @@ app.listen(PORT, () => {
 
 
 
-
-// server.js
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const authRoutes = require("./routes/auth");
-
-// dotenv.config();
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/auth", authRoutes);
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}`);
-// });
--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/auth";
+// const API = "http://localhost:5000/api/auth";
 
 
-// const API = "https://my-auth-api.onrender.com/api/auth";
+const API = "https://api.mibbs.ai/api/auth";
 
 export const registerUser = async (name: string, email: string, password: string) => {
   return axios.post(`${API}/register`, { name, email, password });
